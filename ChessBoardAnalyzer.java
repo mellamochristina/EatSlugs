f43b4e0f6468ac387e98423ddd533ffd61138b0d
mellamochristina <christinavo2509@yahoo.com>
2017-02-18 14:15:59 -0800

    Update ChessBoardAnalyzer.java

--- ChessBoardAnalyzer.java
+++ ChessBoardAnalyzer.java
@@ -100,11 +100,11 @@ class ChessBoardAnalyzer {
   static String analyzeCheckmate() {
 	  //Check all black pieces to make sure they are not attacking white king
 	  //Check all white pieces to make sure they are not attacking black king
-	  	//if a piece is attacking king, print "(color) in check"
+	  	//if a piece is attacking king, return "(color) in check"
 	  //Check if possible destinations for white king are also attacked by black pieces
 	  //Check if possible destinations for black king are also attacked by white pieces
-	  	//if so, print "(color) checkmated"
-	  //if both kings not under attack, print "All kings safe"
+	  	//if so, return "(color) checkmated"
+	  //if both kings not under attack, return "All kings safe"
 	  //If piece at (x’, y’) is blocking and is of the opposite color, this is a check and the piece is removed from list (delete method)
 	  return "Check"; 	  //Return "Black/White in check" OR "Black/White checkmated" OR "All kings safe"
   }
